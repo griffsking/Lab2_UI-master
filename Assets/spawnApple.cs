79e85f80756e4d11969f442232a30f6bcc34ddfe
griffsking <griffking0325@hotmail.com>
2020-02-25 16:52:39 -0500

    Initial commit

---
+++ Assets/spawnApple.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnApple : MonoBehaviour
+{
+
+    public GameObject apple;
+    public void ho()
+    {
+        Vector3 position = new Vector3(Random.Range(-5.36f, 4.99f), 3.5f, 0);
+        Instantiate(apple, position, Quaternion.identity);
+    }
+}
